639a2442751d9c467b208696e4e92686188cec96
Yuntai Kyong <yuntai@una.co.kr>
2020-05-13 17:43:19 +0900

    modify for ML server local environ

--- PyTorch/SpeechSynthesis/Tacotron2/scripts/docker/interactive.sh
+++ PyTorch/SpeechSynthesis/Tacotron2/scripts/docker/interactive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nvidia-docker run --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -it --rm --ipc=host -v $PWD:/workspace/tacotron2/ tacotron2 bash
+docker run --gpus all --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -it --rm --ipc=host -v $PWD:/workspace/tacotron2/ -v /mnt/datasets/LJSpeech-1.1:/workspace/tacotron2/LJSpeech-1.1 -v /mnt/datasets/pretrained:/mnt/datasets/pretrained tacotron2 bash
