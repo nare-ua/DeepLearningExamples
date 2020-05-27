ab607fda64e877572cf3862b484255ae36e3567a
Yuntai Kyong <yuntai@una.co.kr>
2020-05-27 17:33:36 +0900

    adjust dockerfile and script for UA server environ

--- PyTorch/SpeechSynthesis/Tacotron2/scripts/docker/interactive.sh
+++ PyTorch/SpeechSynthesis/Tacotron2/scripts/docker/interactive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --gpus all --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -it --rm -p 10088:10088 --ipc=host -v $PWD:/workspace/tacotron2/ -v /mnt/datasets/LJSpeech-1.1:/workspace/tacotron2/LJSpeech-1.1 -v /mnt/datasets/pretrained:/mnt/datasets/pretrained tacotron2 bash
+docker run --gpus all --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -it --rm -p 8888:10088 --ipc=host -v $PWD:/workspace/tacotron2 -v /mnt/data:/mnt/data tacotron2 bash
