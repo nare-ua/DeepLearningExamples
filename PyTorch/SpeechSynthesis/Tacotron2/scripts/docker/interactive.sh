a44b8e473458db85ade3865df051b4051d305f9e
ua <ua@una.co.kr>
2020-06-02 16:08:52 +0900

    integrate back docker/interactive.sh script

--- PyTorch/SpeechSynthesis/Tacotron2/scripts/docker/interactive.sh
+++ PyTorch/SpeechSynthesis/Tacotron2/scripts/docker/interactive.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-docker run --gpus all --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -it --rm --expose 8888 -p 8888 --ipc=host -v $PWD:/workspace/tacotron2 -v /mnt/data:/mnt/data tacotron2 bash
+set -euxo pipefail
+
+dataroot="/mnt/data"
+if [[ $(hostname) == "nipa2020-0909" ]]; then
+  echo "running on $(hostname)"
+  dataroot="/home/ua/data"
+fi
+echo "dataroot($dataroot)"
+
+docker run --gpus all --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -it --rm --expose 8888 -p 8888 --ipc=host -v $PWD:/workspace/tacotron2 -v ${dataroot}:/mnt/data tacotron2 bash
--- PyTorch/SpeechSynthesis/Tacotron2/scripts/docker/nipa_interactive.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --gpus all --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -it --rm --expose 8888 -p 8888 --ipc=host -v $PWD:/workspace/tacotron2 -v /home/ua/data:/mnt/data tacotron2 bash
