61e0dcc3aa899e7abe14495027c908d9fd33b91a
Yuntai Kyong <yuntai@una.co.kr>
2020-06-05 10:55:18 +0900

    add inference test script

--- PyTorch/SpeechSynthesis/Tacotron2/kor_inf_test.sh
+++
@@ -1,3 +0,0 @@
-for s in 1 2 3 4 5; do
-  python inference.py --tacotron2 checkpoint_Tacotron2_1900 --waveglow /mnt/data/pretrained/tacotron2/waveglow_1076430_14000_amp -o output/ --include-warmup -i phrases/kor/example_$s.txt --amp-run --wn-channels 256 --suffix $s
-done
---
+++ PyTorch/SpeechSynthesis/Tacotron2/scripts/kss/kor_inf_test.sh
@@ -0,0 +1,17 @@
+
+trap ctrl_c INT
+function ctrl_c() {
+  echo "** Trapped CTRL-C"
+  exit 1
+}
+
+epoch=$1
+for s in 1 2 3 4 5; do
+  python inference.py --tacotron2 checkpoint_Tacotron2_${epoch:-1900} --waveglow /mnt/data/pretrained/tacotron2/waveglow_1076430_14000_amp \
+	  -o output/ --include-warmup \
+	  -i phrases/kor/example_$s.txt \
+	  --amp-run --wn-channels 256 \
+	  --sigma-infer 0.6 \
+	  --gate-threshold 0.6 \
+	  --suffix $s
+done
