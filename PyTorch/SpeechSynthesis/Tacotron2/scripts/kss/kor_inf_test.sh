a5ab63c35c6b80e955cd2f2155b4213893563b72
Yuntai Kyong <yuntai@una.co.kr>
2020-06-07 03:12:19 +0900

    add silence removal

--- PyTorch/SpeechSynthesis/Tacotron2/scripts/kss/kor_inf_test.sh
+++ PyTorch/SpeechSynthesis/Tacotron2/scripts/kss/kor_inf_test.sh
@@ -1,4 +1,4 @@
-
+checkpointsdir=/mnt/data/checkpoints/kss
 trap ctrl_c INT
 function ctrl_c() {
   echo "** Trapped CTRL-C"
@@ -7,7 +7,7 @@ function ctrl_c() {
 
 epoch=$1
 for s in 1 2 3 4 5; do
-  python inference.py --tacotron2 checkpoint_Tacotron2_${epoch:-1900} --waveglow /mnt/data/pretrained/tacotron2/waveglow_1076430_14000_amp \
+  python inference.py --tacotron2 $checkpointsdir/checkpoint_Tacotron2_${epoch:-1900} --waveglow /mnt/data/pretrained/tacotron2/waveglow_1076430_14000_amp \
 	  -o output/ --include-warmup \
 	  -i phrases/kor/example_$s.txt \
 	  --amp-run --wn-channels 256 \
