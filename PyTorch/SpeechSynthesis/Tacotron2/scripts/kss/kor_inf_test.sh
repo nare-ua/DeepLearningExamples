0279416c6b1c45432094b37345c183e61638cb82
Yuntai Kyong <yuntai@una.co.kr>
2020-06-09 15:45:26 +0900

    add files for demo app
    and add flask dep into Dockerfile

--- PyTorch/SpeechSynthesis/Tacotron2/scripts/kss/kor_inf_test.sh
+++ PyTorch/SpeechSynthesis/Tacotron2/scripts/kss/kor_inf_test.sh
@@ -1,17 +1,21 @@
-checkpointsdir=/mnt/data/checkpoints/kss
+#!/bin/env bash
+
+set -euo pipefail
+
 trap ctrl_c INT
 function ctrl_c() {
   echo "** Trapped CTRL-C"
   exit 1
 }
 
-epoch=$1
+checkpoint="/mnt/data/checkpoints/kss_ss_1000"
+
 for s in 1 2 3 4 5; do
-  python inference.py --tacotron2 $checkpointsdir/checkpoint_Tacotron2_${epoch:-1900} --waveglow /mnt/data/pretrained/tacotron2/waveglow_1076430_14000_amp \
+  python inference.py --tacotron2 "$checkpoint" --waveglow /mnt/data/pretrained/tacotron2/waveglow_1076430_14000_amp \
 	  -o output/ --include-warmup \
 	  -i phrases/kor/example_$s.txt \
 	  --amp-run --wn-channels 256 \
-	  --sigma-infer 0.6 \
-	  --gate-threshold 0.6 \
+	  --sigma-infer 0.9 \
+	  --gate-threshold 0.5 \
 	  --suffix $s
 done
