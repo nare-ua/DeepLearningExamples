d346c2517ca6d02aa489fe23c643481445c21481
Yuntai Kyong <yuntai@una.co.kr>
2020-07-07 19:30:40 +0900

    consolidate input process script

--- PyTorch/SpeechSynthesis/Tacotron2/scripts/lim/prepare_input.sh
+++ PyTorch/SpeechSynthesis/Tacotron2/scripts/lim/prepare_input.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
-set -e -o pipefail
+set -eux -o pipefail
 
 TMP=/mnt/tmp
 srcroot="/mnt/data/datasets/lim"
 dataroot="$TMP/datasets/lim"
 
-rsync -av $srcroot/* $dataroot
+rm -rf $dataroot
+
+rsync -av --exclude '*.zip' $srcroot/* $dataroot
 
 # relace space in filename with '_'
-for fn in $dataroot/**/*; do mv "$fn" $(echo $fn | tr ' ' '_'); done
+for fn in $dataroot/**/*; do 
+  mv "$fn" $(echo "$fn" | tr ' ' '_')
+done
 
 rm -rf $dataroot/wavs
 mkdir -p $dataroot/wavs
@@ -21,11 +25,31 @@ for fn in $dataroot/**/*.wav $dataroot/**/*.mp3; do
   sox "$fn" -b16 -c1 -r22050 "$outfn" vol 24.31 dB
 done
 
-tmpfilelists=$(mktemp /mnt/tmp/lim.XXXXXXXXX)
-(find $dataroot -name "*.txt" -prune ./wavs -prune -o -print|xargs cat |awk NF|sort) > $tmpfilelists
+filelists_combined=$(mktemp /mnt/tmp/lim.XXXXXXXXX)
+(find $dataroot -path "$dataroot/wavs" -prune -o -name "*.txt" -print|xargs cat|sed 's/\r//'|awk NF|sort) > $filelists_combined
 
-# remove a space after '|
-sed -i 's/| /|/g' $tmpfilelists
+# remove a space after '|'
+sed -i 's/| /|/g' "$filelists_combined"
+
+wavfilelists=$(mktemp /mnt/tmp/lim.XXXXXXXXX)
+melfilelists=$(mktemp /mnt/tmp/lim.XXXXXXXXX)
+
+python <<EOF
+with open('${filelists_combined}', 'r') as inf,\
+  open('${wavfilelists}','w') as outf,\
+  open('${melfilelists}','w') as mel_outf:
+
+  for line in inf:
+    line = line.strip()
+    if line != '':
+      w, t = line.split('|')
+      w = w.strip().replace(' ', '_')
+      t = t.strip()
+      wav_line = "${dataroot}/wavs/" + w + ".wav" + "|" + t
+      mel_line = "${dataroot}/mels/" + w + ".pt" + "|" + t
+      outf.write(wav_line+'\n')
+      mel_outf.write(mel_line+'\n')
+EOF
 
 TRAINLIST="$dataroot/filelists/audio_text_train_filelist.txt"
 VALLIST="$dataroot/filelists/audio_text_val_filelist.txt"
@@ -33,5 +57,15 @@ VALLIST="$dataroot/filelists/audio_text_val_filelist.txt"
 TRAINLIST_MEL="$dataroot/filelists/mel_text_train_filelist.txt"
 VALLIST_MEL="$dataroot/filelists/mel_text_val_filelist.txt"
 
-python preprocess_audio2mel.py --text-cleaners "transliteration_cleaners" --wav-files "$TRAINLIST" --mel-files "$TRAINLIST_MEL"
-python preprocess_audio2mel.py --text-cleaners "transliteration_cleaners" --wav-files "$VALLIST" --mel-files "$VALLIST_MEL"
+mkdir $dataroot/filelists
+
+head -10 $wavfilelists > $VALLIST
+tail -n +11 $wavfilelists > $TRAINLIST
+
+head -10 $melfilelists > $VALLIST_MEL
+tail -n +11 $melfilelists > $TRAINLIST_MEL
+
+rm -f $wavfilelists $melfilelists $filelists_combined
+
+#python preprocess_audio2mel.py --text-cleaners "transliteration_cleaners" --wav-files "$TRAINLIST" --mel-files "$TRAINLIST_MEL"
+#python preprocess_audio2mel.py --text-cleaners "transliteration_cleaners" --wav-files "$VALLIST" --mel-files "$VALLIST_MEL"
